c8c01f4f0cb9707acb8b3d2f296db708e77aff34
Disco <4949083+the-disco-option@users.noreply.github.com>
2025-07-19 15:10:24 +0200

    chore: prettier tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
 import { BuildTargets } from './tabs/build-targets'
 
 // Top level UI goes here
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1 className="text-2xl">Ashes Calculator</h1>
+      <h1>Ashes Calculator</h1>
       <BuildTargets />
     </>
   )
